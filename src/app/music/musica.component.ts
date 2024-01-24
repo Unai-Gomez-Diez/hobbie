cc053cabb8c9eed78e18923709b8474b185ab8c2
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-01-24 13:28:25 +0100

    avanze musica

--- src/app/music/musica.component.ts
+++ src/app/music/musica.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Music} from "../modelos/music.model";
+import {MusicaService} from "../musica.service";
 
 @Component({
   selector: 'app-musica',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./musica.component.css']
 })
 export class MusicaComponent {
+    musicas: Music[] = []
 
+  constructor(private service: MusicaService) {
+  }
+  ngOnInit(){
+      this.service.getMusic().subscribe((musica)=>{
+        this.musicas=musica
+    }
+      )
+  }
 }
